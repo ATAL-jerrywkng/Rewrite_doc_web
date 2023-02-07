2d87c67ec8a1c5ae8d45e46e76b6d8151bd6e714
jerrywkng <jerrywkng@atal.com>
2023-02-07 14:39:39 +0800

    updated new page

--- src/pages/EcrsSystemViewApproval.js
+++ src/pages/EcrsSystemViewApproval.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import img1 from '../asserts/ecApprovalThreeDots.png'
 import img2 from '../asserts/ecApprovalPath.png'
 import img3 from '../asserts/threedots.png'
+import { ContentImage } from '../components/ContentImage';
 
 export const EcrsSystemViewApproval = () => {
   const pageInfo = {
@@ -16,20 +17,21 @@ export const EcrsSystemViewApproval = () => {
     <BasePage  {...pageInfo}>
       <ContentPart title={"查閱「自墊費用」報銷審批歷程"} sx={{ overflow: 'scroll' }}>
 
-      
-        <Typography variant='h6' textAlign={'LEFT'}>1.    點擊需要查閱的自墊費用報銷，並點擊右上角的<CardMedia sx={{ height: '20px', width: '20px', margin: '0' }}image={img3}/>後，再點擊{`<審批歷程>`}</Typography>
-        <CardMedia
-          sx={{ height: '650px', width: '1000px', margin: '0 auto' }}
-          image={img1}
-        />
+        <ol>
+          <li>
 
-        <Typography variant='h6' textAlign={'LEFT'}>2.    自墊費用報銷審批歷程會於此版面顯示</Typography>
-        <CardMedia
-          sx={{ height: '1300px', width: '1300px', margin: '0 auto' }}
-          image={img2}
-        />
+            <Typography variant='subtitle1' textAlign={'LEFT'}>點擊需要查閱的自墊費用報銷，並點擊右上角的<CardMedia sx={{ height: '50px', width: '50px', margin: '0' }} image={img3} />後，再點擊{`<審批歷程>`}</Typography>
+          </li>
 
-        
+          <ContentImage image={img1} />
+
+          <li>
+
+            <Typography variant='subtitle1' textAlign={'LEFT'}>自墊費用報銷審批歷程會於此版面顯示</Typography>
+          </li>
+          <ContentImage image={img2} />
+        </ol>
+
       </ContentPart>
     </BasePage>
   )
