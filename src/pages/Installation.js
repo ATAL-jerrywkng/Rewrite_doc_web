158391acbb8338975923e82ec0b8d4ac89706fd7
jerrywkng <jerrywkng@atal.com>
2023-02-09 17:59:31 +0800

    update

--- src/pages/Installation.js
+++ src/pages/Installation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BasePage } from '../components/BasePage'
 import img1 from '../asserts/img2.png'
-import { CardMedia, Typography } from '@mui/material'
+import { Box, CardMedia, Typography } from '@mui/material'
 import { ContentPart } from '../components/ContentPart';
 import { Link } from 'react-router-dom';
 import { ContentImage } from '../components/ContentImage';
@@ -15,10 +15,12 @@ export const Installation = () => {
   return (
     <BasePage  {...pageInfo}>
       <ContentPart title={"安裝AMA手機應用程式"} sx={{ overflow: 'scroll' }}>
-
-        <Link to="https://apps.apple.com/us/app/atal-mobile-access/id1660210682">
-          <ContentImage image={img1} sx={{ height: '100px', width: '100px' }} />
-        </Link>
+        
+        <Box sx={{ textAlign: 'center' }}>
+          <Link to="https://apps.apple.com/us/app/atal-mobile-access/id1660210682" style={{ display: 'inline-block' }}>
+            <ContentImage image={img1} sx={{ height: '100px', width: '100px' }} />
+          </Link>
+        </Box>
         <Typography variant='h6' textAlign={'center'}>對於手機用戶，請到以下網址或掃描上方的二維碼下載手機應用程式：</Typography>
         <a id="myLink" href="http://ts.atal.com.hk/" target="_blank" ><Typography variant='h6' textAlign={'center'}>http://ts.atal.com.hk/</Typography></a>
         <Typography variant='h6' textAlign={'center'}>* AMA手機應用程式支援iOS和Android平台。</Typography>
