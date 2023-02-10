01b1ca0d0fd1c1d194c917fd1c9d39926ade942e
Chan Kwok Lai <angelachan@atal.com>
2023-02-10 18:13:25 +0800

    [title10]

--- src/pages/Installation.js
+++ src/pages/Installation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BasePage } from '../components/BasePage'
 import img1 from '../asserts/img2.png'
+import img2 from '../asserts/img3.png'
 import { Box, CardMedia, Typography } from '@mui/material'
 import { ContentPart } from '../components/ContentPart';
 import { Link } from 'react-router-dom';
@@ -18,7 +19,10 @@ export const Installation = () => {
         
         <Box sx={{ textAlign: 'center' }}>
           <Link to="https://apps.apple.com/us/app/atal-mobile-access/id1660210682" style={{ display: 'inline-block' }}>
-            <ContentImage image={img1} sx={{ height: '100px', width: '100px' }} />
+            <ContentImage image={img1} sx={{ height: '100px', width: '100px' }} />IOS download
+          </Link>
+          <Link to="https://ts.atal.com.hk/dl/ATALMobileAccess.apk" style={{ display: 'inline-block' }}>
+            <ContentImage image={img2} sx={{ height: '95px', width: '95px' }} />Android download
           </Link>
         </Box>
         <Typography variant='h6' textAlign={'center'}>對於手機用戶，請到以下網址或掃描上方的二維碼下載手機應用程式：</Typography>
